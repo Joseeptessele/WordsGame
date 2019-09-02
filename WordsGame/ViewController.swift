56ada15224b1365c1c58d852d73bb2561fd8386a
Jose Pedron Tessele <josetesselep@gmail.com>
2019-09-01 22:03:15 -0300

    Initial Commit

---
+++ WordsGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordsGame
+//
+//  Created by José Eduardo Pedron Tessele on 01/09/19.
+//  Copyright © 2019 José P Tessele. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
